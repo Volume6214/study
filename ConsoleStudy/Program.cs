f5d67f372f62ea06347f5057fb33274c3bb03e54
Volume <Voluem6214@gmail.com>
2026-04-07 17:11:03 +0900

    [C#] 그룹과외 - 마녀 클래스 만들기.1 (객체의 상호작용)

--- ConsoleStudy/Program.cs
+++ ConsoleStudy/Program.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Server;
+using System;
 
 namespace ConsoleStudy
 {
     internal class Program
     {
+        public class MagicPot // (1)
+        {
+            public void MakePotion(string firstItem, string secondItem)
+            {
+                string collapsedMaterial = $"마법의 포션만들";
+
+                Console.WriteLine($"{firstItem}{secondItem}");
+            }
+        }
+
+        public class Witch // (1)
+        {
+            public void CraftPotion(MagicPot targetMagicPot) // ## 몰랏었음 target 잘 못 씀
+            {
+                targetMagicPot.MakePotion("사탕", "과자");
+
+
+            }
+        }
+
         static void Main(string[] args)
         {
+            MagicPot magicPot = new MagicPot(); // (1)
+            // var magicpot333 = new MagicPot(); // 위아래 같던가? (구분하기편해서)
+            Witch witch = new Witch();
+            // var witch = new Witch();
+
+
+            magicPot.MakePotion("도마뱁 꼬리", "머리카락"); // (1)
+            witch.CraftPotion(magicPot);
 
         }
     }
