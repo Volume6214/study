b6ce4dbe234d7d2198fd04abe9bfc889dfbec057
Volume <Volume6214@gmail.com>
2026-04-14 15:14:10 +0900

    [C#] 그룹과외 프로퍼티 (1)
    Player만들고
    멤버변수로는 ATK있고, HP있고 - > 프로퍼티 응용 가능
    Monster 만들고
    멤버변수로 ATK, HP있고 - > 프로퍼티 응용 가능
    
    둘다 Attack 메서드가 있고,  TakeDamage 메서드가 있고,
    TakeDamage에는 상대의 공격력을 받아와야 함
    
    Main에서는 두 객체가 실체화 될 때 멤버변수를 초기화, 그리고 서로 1대씩 때려서 상대방의
    공격력과 이름을 출력하는 예제

--- StudyCsharp/Program.cs
+++ StudyCsharp/Program.cs
@@ -4,65 +4,47 @@ using System.Threading;
 
 namespace StudyCsharp
 {
-    class Status
-    {
-        public int Atk;
-        public int Hp;
 
-        public Status(int atk, int hp)
-        {
-            Atk = atk;
-            Hp = hp;
-        }
-
-
-    }
     internal class Program
     {
-        class Player
+
+        public class RPGPlayer
         {
 
-            int hp = 100;
-            int atk = 10;
-
-            public void Attack(Monster monster)
-            {
-                Console.WriteLine("플레이어의 공격!");
-                monster.TakeDamage(atk);
-            }
-
-            public void TakeDamage(int damage)
-            {
-                hp -= damage;
-                Console.WriteLine($"플레이어 HP: {hp}");
-            }
+            public string Name { get; set; }
         }
 
-        class Monster
+        public class RPGMonster
         {
 
-            int hp = 50;
-            int atk = 15;
+            public string m_name;
+            private int m_atk;
+            public int Hp { get; set; }
 
-            public void Attack(Player player)
+            public int Atk
             {
-                Console.WriteLine("몬스터의 공격!");
-                player.TakeDamage(atk);
+                get { return m_atk; }
+                set
+                {
+                    if (m_atk != value) 
+                    { 
+                        m_atk = value; 
+                    }
+                }
             }
 
-            public void TakeDamage(int damage)
+            public string GetName()
             {
-                hp -= damage;
-                Console.WriteLine($"몬스터 HP: {hp}");
+                return m_name;
+            }
+
+            public void SetNAme(string name)
+            {
+                m_name = name;
             }
         }
         static void Main(string[] args)
         {
-            Player player01 = new Player();
-            Monster monster01 = new Monster();
-
-            player01.Attack(monster01);
-            monster01.Attack(player01);
 
         }
     }
