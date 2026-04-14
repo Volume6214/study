4801bffc8f92d18a5e2a45ddc0f8521d0279054e
Volume <Volume6214@gmail.com>
2026-04-14 15:06:29 +0900

    [C#] 그룹과외 몬스터 프로퍼티 자습버전

--- StudyCsharp/Program.cs
+++ StudyCsharp/Program.cs
@@ -1,10 +1,69 @@
 ﻿using System;
+using System.Numerics;
+using System.Threading;
 
 namespace StudyCsharp
 {
+    class Status
+    {
+        public int Atk;
+        public int Hp;
 
+        public Status(int atk, int hp)
+        {
+            Atk = atk;
+            Hp = hp;
+        }
+
+
+    }
     internal class Program
     {
+        class Player
+        {
 
+            int hp = 100;
+            int atk = 10;
+
+            public void Attack(Monster monster)
+            {
+                Console.WriteLine("플레이어의 공격!");
+                monster.TakeDamage(atk);
+            }
+
+            public void TakeDamage(int damage)
+            {
+                hp -= damage;
+                Console.WriteLine($"플레이어 HP: {hp}");
+            }
+        }
+
+        class Monster
+        {
+
+            int hp = 50;
+            int atk = 15;
+
+            public void Attack(Player player)
+            {
+                Console.WriteLine("몬스터의 공격!");
+                player.TakeDamage(atk);
+            }
+
+            public void TakeDamage(int damage)
+            {
+                hp -= damage;
+                Console.WriteLine($"몬스터 HP: {hp}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Player player01 = new Player();
+            Monster monster01 = new Monster();
+
+            player01.Attack(monster01);
+            monster01.Attack(player01);
+
+        }
     }
 }
