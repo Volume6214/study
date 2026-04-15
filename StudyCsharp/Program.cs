8c771031251fcbecc9b7cb6bbe6887e16852b374
Volume <Volume6214@gmail.com>
2026-04-15 18:35:57 +0900

    [C#] 제출과제 Action활용 - bool을 활용한 상호작용?
    bool을 이용한 이벤트 사용처
    -------------------------
    1.유저가 슬라이더를 50으로 움직임
    2.입력창도 50으로 변경됨 (텍스트창도 변경 됐으니 여기서 다시한번더 슬라이더한테 이벤트 발생)
    3.무한루프 발생!
    여기서 bool isFromDoAction 넣는다면 텍스트창은 그 변화를 TakeAction으로 받습니다. 이때 isFromDoAction이 false(자동 변화)이므로 return;을 해버려서 다시 슬라이더를 부르는 짓을 멈추게 될거같네요

--- StudyCsharp/Program.cs
+++ StudyCsharp/Program.cs
@@ -2,11 +2,55 @@
 
 namespace StudyCsharp
 {
+    class Player
+    {
+        public string Name;
+
+        public Action<string, bool> OnReact;
+        // 그냥 bool없이하니깐 TakeAction끼리 반응해서 TakeAction이 무한 반복된다.
+
+
+        public void DoAction(string actionName)
+        {
+            Console.WriteLine($"{Name}이(가) {actionName}을 했다.");
+
+            OnReact?.Invoke(actionName, true);
+        }
+
+        public void TakeAction(string actionName, bool isFromDoAction)
+        {
+            Console.WriteLine($"{Name}이(가) {actionName}을 받았다.");
+
+            if (isFromDoAction == false) 
+            {
+                return;
+            } // 여기서 진짜 애먹음 TakeAction이면 false라는 생각으로 do가 아닌 take로 했었음
+
+            Console.WriteLine($"{Name}이(가) {actionName}로 맞대응!");
+
+            OnReact?.Invoke(actionName, false);
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Player player01 = new Player();
+            player01.Name = "플레이어1";
 
+            Player player02 = new Player();
+            player02.Name = "플레이어2";
+
+            // 양방향 연결
+            player01.OnReact += player02.TakeAction;
+            player02.OnReact += player01.TakeAction;
+
+            player01.DoAction("하이파이브");
+
+            Console.WriteLine("------");
+
+            player02.DoAction("손인사");
         }
     }
 }
