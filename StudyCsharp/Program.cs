ded2513c3eb6cd29d7c2edbd69a9cdce008a57de
Volume <Volume6214@gmail.com>
2026-04-15 15:46:12 +0900

    [C#] 그룹과외 11일차 - 단방향 이벤트 먼저 해결
    나중에 더 배우면 TakeDamage를 간소화해보자

--- StudyCsharp/Program.cs
+++ StudyCsharp/Program.cs
@@ -8,6 +8,8 @@ namespace StudyCsharp
         public int Atk { get; private set; }
         public int Hp { get; private set; }
 
+        public Action<int> OnAttack; // (1) 이벤트 서언
+
         public void InitRPGPlayer(string name, int atk, int hp)
         {
             Name = name;
@@ -15,17 +17,21 @@ namespace StudyCsharp
             Hp = hp;
         }
 
-        public void AttackMonster(RPGMonster targetMonster)
+        public void AttackMonster()
         {
-            targetMonster.Hp -= this.Atk;
-            Console.WriteLine($"{this.Name}이 {targetMonster.Name}을" +
-                $" {this.Atk}만큼 공격해서 {targetMonster.Hp}만큼 남았다");
+            Console.WriteLine($"{this.Name}이 공격을 시도했다"); // (1)
+
+            OnAttack?.Invoke(this.Atk);
+
+            /*targetMonster.Hp -= this.Atk;
+            Console.WriteLine($"{this.Name}이 {targetMonster.Name}을" + // (1) 의존성 제거?
+                $" {this.Atk}만큼 공격해서 {targetMonster.Hp}만큼 남았다");*/
         }
 
-        public void TakeDamage(int taregetMonsterAtk)
+        public void TakeDamage(int targetMonsterAtk)
         {
-            this.Hp -= taregetMonsterAtk;
-            Console.WriteLine($"{this.Name}이 {taregetMonsterAtk}의 공격을 받아 {this.Hp}가 남았다");
+            this.Hp -= targetMonsterAtk;
+            Console.WriteLine($"{this.Name}이 {targetMonsterAtk}의 공격을 받아 {this.Hp}가 남았다");
         }
     }
 
@@ -42,15 +48,20 @@ namespace StudyCsharp
             Hp = hp;
         }
 
+        public void TakeDamage(int playerAtk) // (1)
+        {
+            this.Hp -= playerAtk;
+            Console.WriteLine($"{this.Name}이 {playerAtk}만큼 피해를 입어 {this.Hp}가 남았다");
+        }
+
         public void AttackPlayer(RPGPlayer targetPlayer)
         {
             Console.WriteLine($"{this.Name}이 {targetPlayer.Name}을" +
               $" {this.Atk}만큼 공격했다");
             targetPlayer.TakeDamage(this.Atk);
         }
-        }
 
-        public static void StartRPGGame()
+        static void Main(string[] args)
         {
             var player = new RPGPlayer();
             player.InitRPGPlayer("홍길동", 100, 200);
@@ -67,9 +78,11 @@ namespace StudyCsharp
             // 임프의 선공
             monster227.AttackPlayer(player);
 
+            player.OnAttack += monster227.TakeDamage; //(1) 플레이어의 공격에 몬스터가 받도록 한다?
+            // 위 코드는 공격한 것이 아닌 상황부여
 
             // 임프 한번 공격해볼게요
-            player.AttackMonster(monster227);
+            player.AttackMonster(); //플레이어가 공격한다. 따라서 위 조건을 실행하여 데미지를 받는다.
 
 
         }
